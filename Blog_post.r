3041a56a05f136ad48b8c2100bb111cf13b3919b
Mark Edney <m2edney@gmail.com>
2022-08-16 11:55:07 -0400

    republishing site

--- Blog_post.r
+++
@@ -1,8 +0,0 @@
-Blog_post <- function(title){
-date <- Sys.Date()
-name <- paste0(date, '-',title)
-setwd(paste0(getwd(), '/posts'))
-rmarkdown::draft(file = paste0(name), template = "Blog_Post", package = 'rmarkdown')
-file.rename(paste0(name,'/',name, '.Rmd'), paste0(name,'/','index.qmd'))
-setwd('..')
-}
